06c890174ba45cef51c6f6bbb8e3854b9dd7d0b9
abranova <abranova@lsoc.org>
2022-09-16 09:00:38 -0500

    Make changes to shop functions and class

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,25 +7,29 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Sulfuras, Hand of Ragnaros:":
+            if item.name != "Sulfuras, Hand of Ragnaros":
                 if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                     if item.quality > 0 and item.quality <=50:
                         if item.sell_in>=0:
                             item.quality = item.quality - 1
                         else:
                             item.quality = item.quality - 2
-                elif item.name == " Aged Brie":
+                        item.sell_in = item.sell_in - 1
+                elif item.name == "Aged Brie":
                     if item.quality < 50 and item.quality >= 0:
                         item.quality = item.quality + 1
+                    if item.sell_in < 50 and item.sell_in >= 0:
+                        item.sell_in = item.sell_in - 1
                 elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                     if item.sell_in < 0:
                         item.quality = 0
                     else:
                         if item.quality < 50 and item.quality >=0:
-                            if item.sell_in <= 10:
+                            if item.sell_in <= 10 and item.sell_in > 5:
                                 item.quality = item.quality + 2
                             elif item.sell_in <= 5:
                                 item.quality = item.quality + 3
+                        item.sell_in = item.sell_in - 1
 
 
 class Item:
